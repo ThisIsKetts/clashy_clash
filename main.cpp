13c0c3f125e1ddb6397365c0702b9faa2dc454e3
ThisIsKetts <35575182+ThisIsKetts@users.noreply.github.com>
2021-08-08 19:24:30 +0100

    Section 4 Classy Clash - Lesson 58: Summary, Added movement input, Able to move the map, Added raylib math library, used Vector2scale(), Vector2Normalise(), Vector2Subtract()

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -12,6 +13,8 @@ int main()
 
     // load main map
     Texture2D worldMap = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 wmPos{0.0, 0.0};
+    float speed{4.0};
     
 
     SetTargetFPS(60);
@@ -24,8 +27,23 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        
+
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set wmPos = wmPos - direction
+            wmPos = Vector2Subtract(wmPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
+        
+
+
         // draw the world map
-        Vector2 wmPos{0.0, 0.0};
         DrawTextureEx(worldMap, wmPos, 0.0, 4.0, WHITE);
 
         // stop drawing
