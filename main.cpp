4e038db55fc965801d73573cbeffead28a0800a3
ThisIsKetts <35575182+ThisIsKetts@users.noreply.github.com>
2021-08-09 18:03:26 +0100

    Section 4: Classy Clash - Lesson 66. Character Instance, created a Class and moved character texture loading and animation variables into private, added getWorldPos, setScreenPos and tick into public

--- main.cpp
+++ main.cpp
@@ -1,90 +1,114 @@
 #include "raylib.h"
 #include "raymath.h"
 
-int main()
+class Character
 {
+public:
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
 
-    // window dimesnions
-    const int windowWidth{384};
-    const int windowHeight{384};  
-
-    // initialise window
-    InitWindow(windowWidth, windowHeight,"Top Down Game");
-
-    // load main map
-    Texture2D worldMap = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
-    Vector2 wmPos{0.0, 0.0};
-    float speed{4.0};
-
-    // load character sprites
-    Texture2D knight_Idle = LoadTexture("characters/knight_idle_spritesheet.png");
-    Texture2D knight_Run = LoadTexture("characters/knight_run_spritesheet.png");
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-    Vector2 knightPos
-    {
-        (float)windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
-        (float)windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
-    };
-
-
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos;
+    Vector2 worldPos;
     // 1 : facing right, -1 facing left
     float rightLeft{1.f};
     // animation variables
     float runningTime{};
     int frame{};
     const int maxFrames{6};
-    const float updateTime{1.f/12.f};
-    
+    const float updateTime{1.f / 12.f};
+    const float speed{4.f};
+};
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    screenPos = {(float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
+                 (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)
+                };
+};
+
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        // change knight idle animation to run animation if character is moving
+        texture = run;
+    }
+    else
+    {
+        // revert back to idle if character is not moving
+        texture = idle;
+    }
+
+    // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * (float)texture.width / 6.f, 0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
+    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.0f, 4.0f * (float)texture.height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
+
+int main()
+{
+
+    // window dimesnions
+    const int windowWidth{384};
+    const int windowHeight{384};
+
+    // initialise window
+    InitWindow(windowWidth, windowHeight, "Top Down Game");
+
+    // load main map
+    Texture2D worldMap = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 wmPos{0.0, 0.0};
+   
+    Character knight;
+    knight.setScreenPos(windowWidth, windowHeight);
+
 
     SetTargetFPS(60);
-    while(!WindowShouldClose())
+    while (!WindowShouldClose())
     {
-
 
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 direction{};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0;
-        if (Vector2Length(direction) != 0.0)
-        {
-            // set wmPos = wmPos - direction
-            wmPos = Vector2Subtract(wmPos, Vector2Scale(Vector2Normalize(direction), speed));
-            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-            knight = knight_Run;
-        }
-        else
-        {
-            knight = knight_Idle;
-        }
 
-        // update animation frame
-        runningTime += GetFrameTime();
-        if (runningTime >= updateTime)
-        {
-            frame++;
-            runningTime = 0.f;
-            if (frame > maxFrames) frame = 0;
-        }
+        wmPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
         // draw the world map
         DrawTextureEx(worldMap, wmPos, 0.0, 4.0, WHITE);
 
-        // draw the character
-        Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
-        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
-        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
+        knight.tick(GetFrameTime());
+
+
 
         // stop drawing
         EndDrawing();
-
-    } 
-    UnloadTexture(worldMap);
+    }
     CloseWindow();
-
-
 }
