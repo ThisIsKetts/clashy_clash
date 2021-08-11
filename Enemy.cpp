01191409d9a26c558aba0c2e8e9c0d70af7f5ee7
ThisIsKetts <35575182+ThisIsKetts@users.noreply.github.com>
2021-08-11 19:09:58 +0100

    Section 4: Lesson 75 - Inheriting From BaseCharacter, moved members and methods into BaseCharacter and made variables protected,

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,11 +1,12 @@
 #include "Enemy.h"
 
-Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture run_texture) : 
-    texture(idle_texture),
-    idle(idle_texture),
-    run(run_texture),
-    worldPos(pos)
+Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture run_texture)
 {
+    texture = idle_texture;
+    idle = idle_texture;
+    run = run_texture;
+    worldPos = pos;
+
     width = texture.width / maxFrames;
     height = texture.height;
 }
@@ -28,19 +29,4 @@ void Enemy::tick(float deltaTime)
     Rectangle source{frame * width, 0.f, rightLeft * width, height};
     Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
     DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
-
-void Enemy::undoMovement()
-{
-    worldPos = worldPosLastFrame;
-}
-
-Rectangle Enemy::getCollisionRec()
-{
-    return Rectangle{
-    screenPos.x,
-    screenPos.y,
-    width * scale,
-    height * scale
-    };
 }
